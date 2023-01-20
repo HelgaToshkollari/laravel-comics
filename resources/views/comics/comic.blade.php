c80d2a9b771f5565c17a4fb5cf49964f3d5d2186
HelgaToshkollari <toshkollari.helga@gmail.com>
2023-01-20 18:43:34 +0100

    adds footer

--- resources/views/comics/comic.blade.php
+++ resources/views/comics/comic.blade.php
@@ -2,23 +2,29 @@
 
 @section("main-content")
    
-    <div class="row d-flex flex-wrap py-4">
-        <div class="col">
-            <div class="my-comic">
-                @foreach ($comicsList as $comic )
-                <div class="card">
-                    <img class="card-img-top img-fluid" src="{{$comic["thumb"]}}" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">{{$comic["series"]}}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{{$comic["type"]}}</h6>
-                        <p class="card-text">Costo :{{$comic["price"]}}</p>
-                        <p class="card-text">Costo :{{$comic["description"]}}</p>
-                    </div>
-                </div>
-                @endforeach
-            </div>
+<div class="my-bg-cards">
+    <div class="container py-5">
+        <div class="d-flex flex-wrap ">
             
+                @foreach ($comicsList as $comic )
+                    <div class="card-container">
+                        <div class="card-image">
+                            <img class="card-img" src="{{$comic["thumb"]}}" alt="Card image cap">
+                        </div>
+                        <div class="card-body text-white">
+                            <h5 class="card-title">{{$comic["series"]}}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{{$comic["type"]}}</h6>
+                            <p class="card-text">Costo : {{$comic["description"]}}</p>
+                        </div>
+                    </div>
+                @endforeach
+            
+           
+         </div> 
+        <div class="text-center">
+            <button class="btn btn-primary px-4">LOAD MORE</button>
         </div>
     </div>
+</div>
 
 @endsection
